5316f9f8b209a1248f5d04bdfabbae7e2aa271bb
Dharamveer singh <dharamveers162@gmail.com>
2023-07-20 23:04:10 +0530

    bug fixes

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,7 +4,7 @@ import React from "react";
 const Navbar = () => {
   const [open, setOpen] = React.useState(false);
   return (
-    <nav className="px-[6%]  py-5 flex justify-between items-center border-b ">
+    <nav className="px-[6%]  py-5 flex justify-between items-center border-b sticky top-0 bg-white">
       <div className="flex gap-12 items-center">
         <p className="tracking-wide font-bold text-2xl  uppercase">Off Tec</p>
         <div className="md:flex gap-8 text-sm items-center hidden">
@@ -38,17 +38,17 @@ const Navbar = () => {
         ></div>
       </button>
       <menu
-        className={`flex fixed z-50 inset-0 bottom-0 bg-primary bg-opacity-75 backdrop-blur-lg transform  transition-all duration-300 ease-in-out top-20 ${
+        className={`flex fixed z-50 inset-0 bottom-0 bg-white border-t backdrop-blur-lg transform  transition-all duration-300 ease-in-out top-[4.5rem] ${
           open ? " translate-x-0" : " translate-x-full"
         } flex-col  items-center justify-center md:hidden`}
       >
-        <ul className="flex flex-col gap-4 text-3xl text-center mb-20">
+        <ul className="flex flex-col gap-4 text-xl text-center mb-20">
           <li className="tracking-wide font-medium ">Services</li>
           <li className="tracking-wide font-medium ">Projects</li>
           <li className="tracking-wide font-medium ">Clients</li>
           <li className="tracking-wide font-medium ">Pricing</li>
           <li>
-            <button className="border-black border mt-3 px-6 py-3 rounded-2xl font-semibold">
+            <button className="border-black border text-lg  px-6 py-2 rounded-full font-semibold">
               Contact Us
             </button>
           </li>
