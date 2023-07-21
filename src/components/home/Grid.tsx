1b84301cba6a625b467e93f520ffdcf5b3cb1b63
Dharamveer singh <dharamveers162@gmail.com>
2023-07-21 23:00:24 +0530

    bug fixes

--- src/components/home/Grid.tsx
+++ src/components/home/Grid.tsx
@@ -4,7 +4,7 @@ import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 const Grid = () => {
   return (
-    <div className="pb-8 md:pb-14 flex flex-col gap-8   px-[6%]">
+    <div className="pb-8 md:pb-14 flex flex-col gap-4  md:gap-8   px-[6%]">
       <div className="flex flex-col gap-2 ">
         <p className="text-gray-500 text-sm  ">Work</p>
         <div className="flex items-start  md:justify-between  gap-3 md:gap-6 md:items-center flex-col md:flex-row">
@@ -32,34 +32,34 @@ const Grid = () => {
       </div> */}
         </div>
       </div>
-      <Tabs defaultValue="account" className=" w-full  ">
-        <TabsList className="bg-white p-0     ">
+      <Tabs defaultValue="allworks" className=" w-full  ">
+        <TabsList className="bg-white p-0 w-full flex-wrap md:flex-nowrap flex items-start h-auto gap-3 justify-start ">
           <TabsTrigger
-            className=" text-sm !font-normal px-6 py-2  tracking-wide     border-gray-400 w-full data-[state=active]:text-white rounded-full data-[state=active]:border-black   data-[state=active]:bg-red-500   data-[state=active]:shadow-none outline-none cursor-default"
+            className=" text-sm !font-normal px-6 py-2  tracking-wide       border-gray-400  data-[state=active]:text-white rounded-full data-[state=active]:border-black   data-[state=active]:bg-red-500   data-[state=active]:shadow-none outline-none cursor-default"
             value="allworks"
           >
             All works
           </TabsTrigger>
           <TabsTrigger
-            className=" text-sm !font-normal px-6 py-2  tracking-wide     border-gray-400 w-full data-[state=active]:text-white rounded-full data-[state=active]:border-black   data-[state=active]:bg-red-500   data-[state=active]:shadow-none outline-none cursor-default"
+            className=" text-sm !font-normal px-6 py-2  tracking-wide     border-gray-400  data-[state=active]:text-white rounded-full data-[state=active]:border-black   data-[state=active]:bg-red-500   data-[state=active]:shadow-none outline-none cursor-default"
             value="UI/UX Design"
           >
             UI/UX Design
           </TabsTrigger>
           <TabsTrigger
-            className=" text-sm !font-normal px-6 py-2  tracking-wide     border-gray-400 w-full data-[state=active]:text-white rounded-full data-[state=active]:border-black   data-[state=active]:bg-red-500   data-[state=active]:shadow-none outline-none cursor-default"
+            className=" text-sm !font-normal px-6 py-2  tracking-wide     border-gray-400  data-[state=active]:text-white rounded-full data-[state=active]:border-black   data-[state=active]:bg-red-500   data-[state=active]:shadow-none outline-none cursor-default"
             value="BrandIdentity"
           >
             Brand Identity
           </TabsTrigger>
           <TabsTrigger
-            className=" text-sm !font-normal px-6 py-2  tracking-wide     border-gray-400 w-full data-[state=active]:text-white rounded-full data-[state=active]:border-black   data-[state=active]:bg-red-500   data-[state=active]:shadow-none outline-none cursor-default"
+            className=" text-sm !font-normal px-6 py-2  tracking-wide     border-gray-400  data-[state=active]:text-white rounded-full data-[state=active]:border-black   data-[state=active]:bg-red-500   data-[state=active]:shadow-none outline-none cursor-default"
             value="illustration"
           >
             Illustration
           </TabsTrigger>
           <TabsTrigger
-            className=" text-sm !font-normal px-6 py-2  tracking-wide     border-gray-400 w-full data-[state=active]:text-white rounded-full data-[state=active]:border-black   data-[state=active]:bg-red-500   data-[state=active]:shadow-none outline-none cursor-default"
+            className=" text-sm !font-normal px-6 py-2  tracking-wide     border-gray-400  data-[state=active]:text-white rounded-full data-[state=active]:border-black   data-[state=active]:bg-red-500   data-[state=active]:shadow-none outline-none cursor-default"
             value="Development"
           >
             Development
