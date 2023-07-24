182078404294e0c00c57091b612cc07891d01744
Dharamveer singh <dharamveers162@gmail.com>
2023-07-24 22:50:32 +0530

    bug fixes

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,6 +4,25 @@ import React from "react";
 
 const Navbar = () => {
   const [open, setOpen] = React.useState(false);
+  const links = [
+    {
+      name: "Services",
+      path: "/services",
+    },
+    {
+      name: "Projects",
+      path: "/projects",
+    },
+    {
+      name: "Clients",
+      path: "/clients",
+    },
+    {
+      name: "Pricing",
+      path: "/pricing",
+    },
+  ];
+
   return (
     <>
       <nav className="px-[6%]  py-4 flex md:grid  md:grid-cols-12 justify-between z-50 items-center border-b sticky top-0 bg-white">
@@ -11,12 +30,15 @@ const Navbar = () => {
           <p className="tracking-wide font-bold text-2xl">offtec</p>
         </div>
         <div className="md:flex gap-8 text-sm items-center justify-center hidden col-span-8">
-          <p className="tracking-wide font-medium ">Services</p>
-          <p className="tracking-wide font-medium ">Projects</p>
-          <p className="tracking-wide font-medium ">Clients</p>
-          <Link href="/pricing" className="tracking-wide font-medium ">
-            Pricing
-          </Link>
+          {links.map((link) => (
+            <Link
+              href={link.path}
+              key={link.name}
+              className="tracking-wide font-medium "
+            >
+              {link.name}
+            </Link>
+          ))}
         </div>
         <div className="md:flex gap-8 text-sm items-end justify-end hidden col-span-2">
           <Link
@@ -49,10 +71,12 @@ const Navbar = () => {
         } flex-col ml-auto items-center justify-center md:hidden`}
       >
         <ul className="flex flex-col gap-4 text-xl text-center mb-20">
-          <li className="tracking-wide font-medium ">Services</li>
-          <li className="tracking-wide font-medium ">Projects</li>
-          <li className="tracking-wide font-medium ">Clients</li>
-          <li className="tracking-wide font-medium ">Pricing</li>
+          {links.map((link) => (
+            <li key={link.name} className="tracking-wide font-medium ">
+              <Link href={link.path}>{link.name}</Link>
+            </li>
+          ))}
+
           <li>
             <button className="border-black border text-lg  px-6 py-2 rounded-full font-semibold">
               Contact Us
