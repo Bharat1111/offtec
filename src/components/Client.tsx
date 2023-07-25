f28124849d0fc66f3460f97a5c627714bd55b614
Bharat1111 <54268396+Bharat1111@users.noreply.github.com>
2023-07-26 00:00:53 +0530

    client component

--- src/components/Client.tsx
+++ src/components/Client.tsx
@@ -1,30 +1,82 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import Image from "next/image";
 
+const content = {
+  Jason: {
+    name: "Jason Holder",
+    title: "Investor, entrepreneur, Creator",
+    quote: `"Off Tec has come through for me time and time again. Their attention to detail and speed is unmatched."`,
+  },
+  Steve: {
+    name: "Steve Koe",
+    title: "Founder, Keyframe",
+    quote: `"OFF Tec helped me turn a small idea into an entire brand and story. They did it in just 3 weeks."`,
+  },
+  David: {
+    name: "David Holder",
+    title: "Managing partner, Brand federation",
+    quote: `"They moved faster than us. And we thought we were working hard. How they deliver such high quality and do it so quickly is a mystery."`,
+  },
+};
+
 const Client = () => {
+  const [active, setActive] = useState<"Jason" | "Steve" | "David">("David");
   return (
     <div
-      className="px-[6%] py-3 min-h-[90vh] flex flex-col bg-black text-white justify-between"
+      className="px-[6%] py-[5%] min-h-[90vh] flex flex-col bg-black text-white justify-between"
       id="clients"
     >
-      <h1 className="text-5xl uppercase">
-        &quot;Off Tec has come through for me time and time again. Their
-        attention to detail and speed is unmatched.&quot;
+      <h1 className="text-5xl uppercase font-medium">
+        {content[active].quote}
       </h1>
       <div>
-        <Image
-          src={
-            "https://uploads-ssl.webflow.com/63e3f7ccdb2e2a288a3cb48a/643ef6f7a4c95c4efc825b09_sahil%403x.jpg"
-          }
-          alt="Jason"
-          className="rounded-full mb-8"
-          loading="lazy"
-          width={80}
-          height={80}
-        />
-        <div className="text-size-eyebrow">— Jason Holder</div>
+        <div className="flex gap-3">
+          <Image
+            src={
+              "https://uploads-ssl.webflow.com/63e3f7ccdb2e2a288a3cb48a/643ef6f7a4c95c4efc825b09_sahil%403x.jpg"
+            }
+            onClick={() => setActive("Jason")}
+            alt="Jason"
+            // blur when inactive
+            className={`rounded-full mb-10 ${
+              active === "Jason" ? "" : "opacity-50"
+            } hover:opacity-100 cursor-pointer`}
+            loading="lazy"
+            width={70}
+            height={70}
+          />
+          <Image
+            src={
+              "https://uploads-ssl.webflow.com/63e3f7ccdb2e2a288a3cb48a/643ef7345969348f14926111_dan%403x.jpg"
+            }
+            onClick={() => setActive("Steve")}
+            alt="Steve"
+            className={`rounded-full mb-10 ${
+              active === "Steve" ? "" : "opacity-50"
+            } hover:opacity-100 cursor-pointer`}
+            loading="lazy"
+            width={70}
+            height={70}
+          />
+          <Image
+            src={
+              "https://uploads-ssl.webflow.com/63e3f7ccdb2e2a288a3cb48a/643f3f04da838457f4e50077_Screenshot%202023-04-18%20at%208.08.03%20PM-p-500.png"
+            }
+            onClick={() => setActive("David")}
+            alt="David"
+            className={`rounded-full mb-10 ${
+              active === "David" ? "" : "opacity-50"
+            } hover:opacity-100 cursor-pointer`}
+            loading="lazy"
+            width={70}
+            height={70}
+          />
+        </div>
+
+        <div className="text-size-eyebrow">— {content[active].name}</div>
         <div className="text-size-eyebrow text-gray-500">
-          Investor, entrepreneur,&nbsp;Creator
+          {content[active].title}
         </div>
       </div>
     </div>
