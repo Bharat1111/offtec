005219b0cfd6dead92e5b0bc3825d6cf1b7b12af
Bharat1111 <54268396+Bharat1111@users.noreply.github.com>
2023-07-26 23:33:57 +0530

    services

--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -21,7 +21,7 @@ const ServicesWrapper = ({ children }: { children: React.ReactNode }) => {
     <>
       <div className="md:w-6/12 hidden md:block">
         <h1 className="text-8xl  font-bold mb-6 uppercase break-words">
-          {active === "" ? "Sevices" : active}
+          {active === "" ? "Services" : active}
         </h1>
       </div>
       <div className="md:w-6/12">
