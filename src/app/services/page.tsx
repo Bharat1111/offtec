8a0e3396508400053c8f69a90c6ce30caacdf733
Dharamveer singh <dharamveers162@gmail.com>
2023-07-26 23:00:14 +0530

    services

---
+++ src/app/services/page.tsx
@@ -0,0 +1,55 @@
+import React from "react";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+import ServicesWrapper from "@/components/Services";
+
+const Server = () => {
+  return (
+    <main className=" py-3 md:py-10 px-[6%] flex flex-col md:flex-row gap-10">
+      <ServicesWrapper>
+        <AccordionItem value="Front End">
+          <AccordionTrigger>Front End</AccordionTrigger>
+          <AccordionContent>
+            Yes. It adheres to the WAI-ARIA design pattern.
+          </AccordionContent>
+        </AccordionItem>
+        <AccordionItem value="Creative Development">
+          <AccordionTrigger>Creative Development</AccordionTrigger>
+          <AccordionContent>
+            Yes. It adheres to the WAI-ARIA design pattern.
+          </AccordionContent>
+        </AccordionItem>
+        <AccordionItem value="Anima tion">
+          <AccordionTrigger>Animation</AccordionTrigger>
+          <AccordionContent>
+            Yes. It adheres to the WAI-ARIA design pattern.
+          </AccordionContent>
+        </AccordionItem>
+        <AccordionItem value="Landing pages">
+          <AccordionTrigger>Landing pages</AccordionTrigger>
+          <AccordionContent>
+            Yes. It adheres to the WAI-ARIA design pattern.
+          </AccordionContent>
+        </AccordionItem>
+        <AccordionItem value="Perform ance optimiz ation">
+          <AccordionTrigger>Performance optimization</AccordionTrigger>
+          <AccordionContent>
+            Yes. It adheres to the WAI-ARIA design pattern.
+          </AccordionContent>
+        </AccordionItem>
+        <AccordionItem value="Animat ion2">
+          <AccordionTrigger>Animation</AccordionTrigger>
+          <AccordionContent>
+            Yes. It adheres to the WAI-ARIA design pattern.
+          </AccordionContent>
+        </AccordionItem>
+      </ServicesWrapper>
+    </main>
+  );
+};
+
+export default Server;
---
+++ src/components/Services.tsx
@@ -0,0 +1,38 @@
+"use client";
+import React, { useState } from "react";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+const ServicesWrapper = ({ children }: { children: React.ReactNode }) => {
+  const [active, setActive] = useState<
+    | "Front End"
+    | "Creative Development"
+    | "Anima tion"
+    | "Landing pages"
+    | "Perform ance optimiz ation"
+    | "Animat ion2"
+  >("Front End");
+  return (
+    <>
+      <div className="md:w-6/12 hidden md:block">
+        <h1 className="text-8xl  font-bold mb-6 uppercase break-words">
+          {active}
+        </h1>
+      </div>
+      <div className="md:w-6/12">
+        <Accordion
+          type="single"
+          collapsible
+          onValueChange={(value) => setActive(value as any)}
+        >
+          {children}
+        </Accordion>
+      </div>
+    </>
+  );
+};
+
+export default ServicesWrapper;
