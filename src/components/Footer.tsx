517295a64357771f304732a728a279d6b4013ea1
Bharat1111 <54268396+Bharat1111@users.noreply.github.com>
2023-07-29 23:00:29 +0530

    subscribe firebase

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,9 @@
+"use client";
+
+import { addDoc, collection } from "firebase/firestore";
 import Link from "next/link";
 import React from "react";
+import { useForm } from "react-hook-form";
 import { AiOutlineUser } from "react-icons/ai";
 import {
   FaTwitter,
@@ -7,31 +11,98 @@ import {
   FaLinkedinIn,
   FaFacebookF,
 } from "react-icons/fa";
+import { db } from "@/lib/firebaseconfig";
 
 const Footer = () => {
+  const {
+    register,
+    control,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm();
+
+  const [loading, setLoading] = React.useState(false);
+  const [success, setSuccess] = React.useState(false);
+
+  const addData = async (data: any) => {
+    setLoading(true);
+    try {
+      const docRef = await addDoc(collection(db, "newsletter"), data);
+      console.log("Document written with ID: ", docRef.id);
+      setSuccess(true);
+      reset();
+      setLoading(false);
+      setTimeout(() => {
+        setSuccess(false);
+      }, 5000);
+    } catch (e) {
+      console.error("Error adding document: ", e);
+      setLoading(false);
+    }
+  };
+
   return (
     <footer className="px-[6%] container bg-black py-8   items-center gap-6 md:gap-0 justify-center  flex flex-col md:grid md:grid-cols-12   md:justify-around  text-white border-t border-t-slate-100">
       <div className="col-span-2">
-        <h1 className="text-base font-bold text-gray-500 pb-3">Off-Tec</h1>
+        <h1 className="text-base font-bold text-gray-500 pb-3 uppercase">
+          Off-Tec
+        </h1>
         <p className="text-xl uppercase tracking-wide">Services</p>
         <p className="text-xl uppercase tracking-wide">Projects</p>
         <p className="text-xl uppercase tracking-wide">Pricing</p>
       </div>
 
-      <div className="flex gap-3 col-span-8 justify-center">
-        <div className="border px-6 text-xs py-1.5 flex gap-3 items-center rounded-full">
-          <AiOutlineUser className="text-xl" />
+      <form
+        className="flex gap-3 col-span-8 justify-center"
+        onSubmit={handleSubmit((data) => {
+          addData(data);
+        })}
+      >
+        <div className="border px-6 text-lg py-3 flex gap-3 items-center rounded-full">
           <input
-            type="text"
+            type="email"
             placeholder="Enter your email"
             className="outline-none bg-transparent w-full"
+            {...register("email", {
+              required: true,
+              pattern: /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/,
+            })}
           />
         </div>
-        <button className="bg-white text-black px-6 py-1.5 rounded-full flex gap-2 items-center">
-          <p className="text-xs font-semibold">Subscribe</p>
+        <button
+          type="submit"
+          className="bg-white text-black px-6 py-3 rounded-full flex gap-2 items-center"
+        >
+          {loading ? (
+            <svg
+              className="animate-spin  h-5 w-5 text-black"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+            >
+              <circle
+                className="opacity-25"
+                cx="12"
+                cy="12"
+                r="10"
+                stroke="currentColor"
+                strokeWidth="4"
+              ></circle>
+              <path
+                className="opacity-75"
+                fill="currentColor"
+                d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4z"
+              ></path>
+            </svg>
+          ) : success ? (
+            <p className="text-xs font-semibold">Subscribed</p>
+          ) : (
+            <p className="text-xs font-semibold">Subscribe</p>
+          )}
           {/* <BsArrowRightShort /> */}
         </button>
-      </div>
+      </form>
 
       <div className="flex flex-col col-span-2">
         <div className="flex gap-3">
