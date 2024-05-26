65638828dbe80989a6ff80bda0d69379887dc8b2
bharat1111 <54268396+Bharat1111@users.noreply.github.com>
2024-05-26 23:18:20 +0530

    main component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,42 +1,19 @@
-import Image from "next/image";
-import Link from "next/link";
-import mainImg from "@/assets/main.jpg";
-import Services from "@/components/home/Services";
-import Grid from "@/components/home/Grid";
-import Client from "@/components/Client";
-
 export default function Home() {
   return (
-    <main className="flex  flex-col   ">
-      <div className="flex flex-col gap-2 py-8 md:py-14 px-[6%]">
-        <p className="text-gray-500 text-sm ml-2 ">
-          Introducing a new era of creativity
-        </p>
-        <div>
-          <h1 className="text-7xl md:text-8xl font-semibold ">
-            OffTech Digital
-            <div className="flex gap-x-48 flex-col md:flex-row items-start justify-between md:items-center">
-              <p className="">Agency </p>
-              <Link
-                className="bg-black justify-center text-sm font-medium text-center text-white rounded-full py-3  px-8 mt-8"
-                href="/hireus"
-              >
-                Lets Talk 👋🏻
-              </Link>
-            </div>
-          </h1>
+    <main className="flex flex-col">
+      <div className="p-20">
+        <div className="mx-10 mb-20 hidden md:flex md:flex-col">
+          <h1 className="text-6xl font-semibold line-clamp-1 text-center pb-2">Shaping Tomorrow's Digital</h1>
+          <h1 className="text-6xl font-semibold line-clamp-1 text-center pb-2">Landscape with Unmatched</h1>
+          <h1 className="text-6xl font-semibold line-clamp-1 text-center pb-2">Creativity and Precision.</h1>
         </div>
+        <h1 className="md:hidden">
+          Shaping Tomorrow's Digital Landscape with Unmatched Creativity and
+          Precision.
+        </h1>
+
+        <div className="mx-72">Hello Robo. The top-rated design partner in helping fast-growing companies solve business challenges through design.</div>
       </div>
-      <div className="">
-        <Image
-          src={mainImg}
-          alt="Picture of the author"
-          className="w-full aspect-[16/15] md:aspect-[16/6]  object-cover "
-        />
-      </div>
-      <Services />
-      {/* <Grid /> */}
-      <Client />
     </main>
   );
 }
