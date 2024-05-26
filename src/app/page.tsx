33047031fc6cc6b5a8669326b918d06df7ba680e
bharat1111 <54268396+Bharat1111@users.noreply.github.com>
2024-05-26 23:32:51 +0530

    deployment bug

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,12 @@ export default function Home() {
     <main className="flex flex-col">
       <div className="p-20">
         <div className="mx-10 mb-20 hidden md:flex md:flex-col">
-          <h1 className="text-6xl font-semibold line-clamp-1 text-center pb-2">Shaping Tomorrow's Digital</h1>
+          <h1 className="text-6xl font-semibold line-clamp-1 text-center pb-2">Shaping Tomorrow&apos;s Digital</h1>
           <h1 className="text-6xl font-semibold line-clamp-1 text-center pb-2">Landscape with Unmatched</h1>
           <h1 className="text-6xl font-semibold line-clamp-1 text-center pb-2">Creativity and Precision.</h1>
         </div>
         <h1 className="md:hidden">
-          Shaping Tomorrow's Digital Landscape with Unmatched Creativity and
+          Shaping Tomorrow&apos;s Digital Landscape with Unmatched Creativity and
           Precision.
         </h1>
 
--- src/components/HireUs.tsx
+++ src/components/HireUs.tsx
@@ -7,7 +7,7 @@ const HireUs = () => {
       href={`/hireus`}
       className="p-5 flex bg-[#1d1d1f] items-center text-white rounded-full gap-x-8"
     >
-      <p className="text-[11px] font-semibold">Let's talk</p>
+      <p className="text-[11px] font-semibold">Let&apos;s talk</p>
       <div className="bg-white w-1 h-1 rounded-full"></div>
     </Link>
   );
